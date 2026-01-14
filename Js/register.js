3a822480a485eec565c038df3b2fe3afdba2e8d5
ita-25 <impacttechacademy@gmail.com>
2026-01-14 03:09:22 +0100

    Remove Supabase initialization comment
    Removed commented line about Supabase initialization.

--- Js/register.js
+++ Js/register.js
@@ -1,4 +1,3 @@
-// Initialize Supabase with YOUR credentials
         const supabaseUrl = 'https://dvptsrzfbmrbexcwhkqe.supabase.co';
         const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR2cHRzcnpmYm1yYmV4Y3doa3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxOTg0NTUsImV4cCI6MjA4Mzc3NDQ1NX0.vsimT5xjZ9i_hgqExebBUaHUbGCC9zstak__tly4u4Y';
         const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
